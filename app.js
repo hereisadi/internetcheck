bf01c7c3d69cc0611c07e30ca4f3afea0ed2541e
hereisadi <hereisadi@proton.me>
2024-03-20 18:24:29 +0530

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
